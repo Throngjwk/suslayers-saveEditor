ac80521bd4ce9227be92e1d19d5aa52408271e7b
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-05-30 08:42:46 -0300

    Add files via upload

---
+++ sus.js
@@ -0,0 +1,7 @@
+let name = "Sussy Layers - Save Game Editor";
+
+document.getElementById('sus').onclick = () => {
+    alert('connection sussy timed out.')
+};
+
+document.getElementById('title').innerText = name;
